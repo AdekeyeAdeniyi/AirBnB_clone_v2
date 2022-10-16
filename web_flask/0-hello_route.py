7e09fb498c0a713dabb231eefc3f03ea59ad07a4
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-17 00:15:53 +0100

    Flask app setup

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,9 @@ from flask import Flask
 
 app=Flask(__name__)
 
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Display a message"""
-    return 'Hello HBNB!'
+    return 'Hello HBNB'
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
