fc86600333992702e1c227f0201c88bbd2e643f3
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-17 00:18:13 +0100

    Flask web application setup

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 """
-    Script to start a Flask Web Application
+Start a Flask Web Application
 """
 from flask import Flask
-
 app=Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
