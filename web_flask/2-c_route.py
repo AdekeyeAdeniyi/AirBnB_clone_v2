474a61aa3ce856a9cc80dea4da469c49382c64b2
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-17 00:46:49 +0100

    Flask rout and variable

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def message(text):
     """ Display C with text variable"""
-    return 'User {}'.format(text).replace('_', ' ')
+    return 'C {}'.format(text).replace('_', ' ')
 
 
 if __name__ == '__main__':
