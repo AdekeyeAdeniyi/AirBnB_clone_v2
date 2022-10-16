090e1673cc97d51e9c1ae40ca4de508eeb9a2c03
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-17 00:26:15 +0100

    Flask web application setup

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,12 +3,13 @@
 Start a Flask Web Application
 """
 from flask import Flask
-app=Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB'
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
