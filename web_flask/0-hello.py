ce40f6c3b04c160a14be3b4c1b73e2083ba7e9c4
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-17 00:08:52 +0100

    Flask app setup

--- web_flask/0-hello.py
+++ web_flask/0-hello.py
@@ -7,8 +7,8 @@ app=Flask(__name__)
 
 app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Print a message"""
+    """Display a message"""
     return 'Hello HBNB!'
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=500)
+    app.run(debug=True)
